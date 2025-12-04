ed88264a3365ad3d279d8c878fbbc3fa0703f774
Guilherme Maranhão <guilherme.maranhao@qintess.com>
2025-12-04 11:17:10 -0300

    fix: removed unecessary definitions in request

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,15 +1,14 @@
-import { PokemonListResponse } from "../types/pokemon";
 import axiosClient from "./api";
 
 export const getPokemonList = async (
   offset: number = 0,
   limit: number = 12
-): Promise<PokemonListResponse> => {
+): Promise<any> => {
   try {
-    const response = await axiosClient.get<PokemonListResponse>(
+    const response = await axiosClient.get<any>(
       `pokemon?offset=${offset}&limit=${limit}`
     );
-    return response as unknown as PokemonListResponse;
+    return response;
   } catch (error) {
     throw error;
   }
@@ -20,7 +19,7 @@ export const getPokemonDetails = async (
 ): Promise<any> => {
   try {
     const response = await axiosClient.get<any>(`pokemon/${nameOrId}`);
-    return response as unknown as any;
+    return response;
   } catch (error) {
     throw error;
   }
@@ -29,7 +28,7 @@ export const getPokemonDetails = async (
 export const getPokemonTypes = async (): Promise<any> => {
   try {
     const response = await axiosClient.get<any>(`type`);
-    return response as unknown as any;
+    return response;
   } catch (error) {
     throw error;
   }
@@ -38,7 +37,7 @@ export const getPokemonTypes = async (): Promise<any> => {
 export const getPokemonsByType = async (type: string): Promise<any> => {
   try {
     const response = await axiosClient.get<any>(`type/${type}`);
-    return response as unknown as any;
+    return response;
   } catch (error) {
     throw error;
   }
